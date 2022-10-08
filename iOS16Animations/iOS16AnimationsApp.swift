b2696acb3be05700111f3e7417ba6cb4f1e94dec
Amos Gyamfi <amos.gyamfi@getstream.io>
2022-10-08 18:14:10 +0300

    Initial Commit

---
+++ iOS16Animations/iOS16AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS16AnimationsApp.swift
+//  iOS16Animations
+//
+//  Created by amos.gyamfi@getstream.io on 8.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct iOS16AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
